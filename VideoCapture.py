9d4b81003eb46aa88bdd9dde8ac16a8a34df3141
yoko <miyatakeyouko@gmail.com>
2019-10-08 13:00:15 +0800

    Save the picture

--- VideoCapture.py
+++ VideoCapture.py
@@ -32,29 +32,41 @@ while(True):
     centerpoint = False
     for c in cnts:
         area = cv2.contourArea(c)
-        if(area > 4000):
+        if area > 4000:
+        
+            # Calculate the center of gravity
             M = cv2.moments(c)
             if not M["m00"] == 0:
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
+                
                 #cv2.drawContours(img_contour, [c], -1, (255, 0, 0), 3)
                 
                 # Draw a dot on the center point
                 cv2.circle(gscale, (cX, cY), 3, (0, 0, 255), -1)
                 
+                # Add coordinate info to the array
                 center.append([cX, cY])
+                
                 centerpoint = True
                 
         #cv2.imshow('img_contour', img_contour)
         cv2.imshow('gscale', gscale)
-    if not centerpoint:
-        center.clear()
-        #ret, img_record = cv2.threshold(gscale, 255, 255, cv2.THRESH_BINARY)
 
+    # Draw white circle
     for row in range(len(center)):
         cv2.circle(img_record, (center[row][0], center[row][1]), 15, (255, 255, 255), -1)
     
     cv2.imshow('img_record', img_record)
+    
+    # If the blight part disappear
+    if not centerpoint:
+        #
+        cv2.imwrite("num.jpg", img_record)
+        
+        # Clear the record
+        center.clear()
+        #ret, img_record = cv2.threshold(gscale, 255, 255, cv2.THRESH_BINARY)
 
     # 若按下 q 鍵則離開迴圈
     if cv2.waitKey(1) & 0xFF == ord('q'):
